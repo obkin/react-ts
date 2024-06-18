08e45c24433bce4fd3d51f5a0b7073e8406097c1
obkin <kapellwork@gmail.com>
2024-06-18 17:00:28 +0300

    Drug & Drop types

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -2,19 +2,48 @@ import React, { FC, useState } from 'react';
 
 const EventsExample: FC = () => {
     const [value, setValue] = useState<string>('');
+    const [drug, setDrug] = useState<boolean>(false);
 
     const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         setValue(e.target.value);
     }
 
     const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
         console.log(value);
+    }
+
+    const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        console.log('WAS DRAG');
+    }
+
+    const dropHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setDrug(false);
+    }
+
+    const leaveHandlaer = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setDrug(false);
+    }
+
+    const dragWithPreventHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setDrug(true);
     }
 
     return (
         <div style={{ margin: '20px' }}>
             <input value={value} onChange={changeHandler} type="text" />
             <button onClick={clickHandler}>click</button>
+            <div draggable onDrag={dragHandler} style={{ width: '200px', height: '200px', background: 'red', marginTop: '15px' }}></div>
+            <div 
+                onDrop={dropHandler}
+                onDragLeave={leaveHandlaer}
+                onDragOver={dragWithPreventHandler}
+                style={{ width: '200px', height: '200px', background: drug ? 'blue' : 'red', marginTop: '15px' }}>
+            </div>
         </div>
     );
 };
