bd8cfa72cce4c1595ae5291ff58243a29f6834a4
obkin <kapellwork@gmail.com>
2024-06-21 14:37:07 +0300

    UserItem & TodoItem pages open after click

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,13 +1,16 @@
 import React, { FC } from 'react';
 import { ITodo } from '../types/types';
+import { useNavigate } from 'react-router-dom';
 
 interface ITodoItemProps {
     todo: ITodo;
 }
 
 const TodoItem: FC<ITodoItemProps> = ({ todo }) => {
+    const navigator = useNavigate();
+
     return (
-        <div>
+        <div onClick={() => navigator(`/todos/${todo.id}`)}>
             <input type="checkbox" checked={todo.completed} />
             {todo.id}. {todo.title}
         </div>
--- src/components/UserItem.tsx
+++ src/components/UserItem.tsx
@@ -1,13 +1,16 @@
 import React, { FC } from 'react';
 import { IUser } from '../types/types';
+import { useNavigate } from 'react-router-dom';
 
 interface IUserItemProps {
     user: IUser;
 }
 
 const UserItem: FC<IUserItemProps> = ({ user }) => {
+    const navigator = useNavigate();
+
     return (
-        <div style={{ padding: '15px', border: '1px solid grey' }}>
+        <div onClick={() => navigator(`/users/${user.id}`)} style={{ padding: '15px', border: '1px solid grey' }}>
             {user.name} lives in {user.address.city} in the {user.address.street} street (email: {user.email}).
         </div>
     );
